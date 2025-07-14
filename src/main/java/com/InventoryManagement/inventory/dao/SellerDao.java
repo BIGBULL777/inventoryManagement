acecae8aea0e61743b0da1b3e09f5fd0ab2c0ad7
Kaushik <kaushikp0908@gmail.com>
2025-07-14 21:40:30 +0530

    Added @repo annotation

--- src/main/java/com/InventoryManagement/inventory/dao/SellerDao.java
+++ src/main/java/com/InventoryManagement/inventory/dao/SellerDao.java
@@ -2,6 +2,8 @@ package com.InventoryManagement.inventory.dao;
 
 import com.InventoryManagement.inventory.Entities.Seller;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SellerDao extends CrudRepository<Seller, String> {
 }
